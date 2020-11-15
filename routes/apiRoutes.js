088d39b95a8645b7f3d71038c8a7b948e25bef95
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-14 19:31:11 -0600

    modified router post command

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,6 +10,10 @@ router.get('/notes', (req, res) => {
     res.json(dataBase)
 }) ;
 
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+      
+  });
 
 
 module.exports = router
