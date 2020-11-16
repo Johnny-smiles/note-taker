a09153706e2866a25ed785a057b73456efb951c7
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-16 12:05:06 -0600

    Refactored post code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,37 @@
 // node module path
-const { Router } = require("express");
-const path = require("path");
 // init router
 const router = require("express").Router();
+// unique id package
+const uuidv1 = require("uuidv1");
+// linking fs to write files
+const fs = require('fs');
+// linking util for filesystem write fuction
+const util = require('util');
 // liking database
 let dataBase = require('../db/db.json');
+// linking util and file system to uses promises
+const writeFilAsync = util.promisify(fs.writeFile);
 
 router.get('/notes', (req, res) => {
     res.json(dataBase)
 }) ;
 
-function createNewNote(body, noteArray) {
-  const note = body;
-  fs.writeFileSync(
-    path.join(__dirname, '../db/db.json'),
-    JSON.stringify({ note: noteArray }, null, 2)
-  );
-  return note;
-}
-
 router.post('/notes', (req, res) => {
-    const note = createNewNote(req.body, note);
+  // assigning request
+  let note = req.body;
+  // generating key
+  let id = uuidv1();
+  // attaching it to note
+  note.id = id;
+  // pushing new note to database
+  dataBase.push(note);
+  // writing file to database
+  writeFilAsync('db/db.json', JSON.stringify(dataBase)).then(()=> {
     res.json(note);
+  }).catch(err =>{
+    console.log(err)
+  })
+
   });
 
 
