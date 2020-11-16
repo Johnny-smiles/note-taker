04b636ca182807faf31601dae5544524861448d8
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-15 22:53:42 -0600

    modified api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,9 +10,18 @@ router.get('/notes', (req, res) => {
     res.json(dataBase)
 }) ;
 
+function createNewNote(body, noteArray) {
+  const note = body;
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ note: noteArray }, null, 2)
+  );
+  return note;
+}
+
 router.post('/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-      
+    const note = createNewNote(req.body, note);
+    res.json(note);
   });
 
 
